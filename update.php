31c341dc0288964bf533638b56fe1a659c9ed3c1
yAsmin241 <ybzahiruddin@gmail.com>
2024-11-15 06:01:11 +0800

    Update update.php

--- update.php
+++ update.php
@@ -13,38 +13,46 @@ if(!isset ($_SESSION['username'])){
     <link rel="stylesheet"  href="style.css">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
 
 </head>
 
 <body>
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="#">
+      <i class="bi bi-list"></i> <!-- This replaces the "Menu" text with a list icon -->
+    </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="https://www.utm.my/about/vision-mission/">ABOUT</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
+          <a class="nav-link" href="https://www.utm.my/about/achievements/">STUDY AT UTM</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown link
+            FACULTIES
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><a class="dropdown-item" href="https://comp.utm.my/?_gl=1%2A19au12w%2A_ga%2ANDExMDc0MTI0LjE2OTU2NTY4Mzg.%2A_ga_N3HJW8G3P7%2AMTczMTYxMTU1Mi4zNy4xLjE3MzE2MTE5MzQuNC4wLjA.">
+            Faculty of Computing</a></li>
+            <li><a class="dropdown-item" href="https://mech.utm.my/?_gl=1%2Ag57d33%2A_ga%2ANDExMDc0MTI0LjE2OTU2NTY4Mzg.%2A_ga_N3HJW8G3P7%2AMTczMTYxMTU1Mi4zNy4xLjE3MzE2MTIwNjAuMzQuMC4w">
+            Faculty of Mechanical Engineering</a></li>
+            <li><a class="dropdown-item" href="https://fke.utm.my/?_gl=1%2A1omlqnk%2A_ga%2ANDExMDc0MTI0LjE2OTU2NTY4Mzg.%2A_ga_N3HJW8G3P7%2AMTczMTYxMTU1Mi4zNy4xLjE3MzE2MTIxMzkuNjAuMC4w">
+            Faculty of Electrical Engineering</a></li>
           </ul>
+      </ul>
+      </ul>
         </li>
       </ul>
+      <a href="logout.php" style="color: red; font-size: 16px; text-decoration: none; text-align: right;">Logout</a>
+
+      <img src="logo_utm.png" alt="UTM Logo" style="height: 100px; margin-left: auto;">
     </div>
   </div>
 </nav>
@@ -73,7 +81,7 @@ if(!isset ($_SESSION['username'])){
             $sql = "UPDATE students SET name='$name', email='$email', programme='$programme', year='$year' WHERE id='$id'";
     
             if(mysqli_query ($conn, $sql)){
-                echo "<div class='success-record'>Record is successfully updated!</div>";
+                echo "<div class='success-record'>Student record is successfully updated!</div>";
             } else{
                 echo "Error: ".sql. "<br>" . mysqli_error($conn); 
             }
@@ -82,27 +90,28 @@ if(!isset ($_SESSION['username'])){
     
     <div class="container form-container">
         <h2 class="form-title">Update Students</h2>
-        <form>
+        <form method="POST">
             <div class="mb-3">
                 <label for="name" class="form-label">Name:</label>
-                <input type="text" class="form-control" id="name" placeholder="Enter student name" required>
+                <input type="text" class="form-control" id="name" name="name" placeholder="Enter student name" required>
             </div>
             <div class="mb-3">
                 <label for="email" class="form-label">Email:</label>
-                <input type="email" class="form-control" id="email" placeholder="Enter student email" required>
+                <input type="email" class="form-control" id="email" name="email" placeholder="Enter student email" required>
             </div>
             <div class="mb-3">
                 <label for="program" class="form-label">Program of Study:</label>
-                <select class="form-select" id="program">
-                    <option value="data-engineering" selected>Data Engineering</option>
-                    <option value="computer-science">Computer Science</option>
-                    <option value="information-technology">Information Technology</option>
-                    <!-- Add more options as needed -->
+                <select class="form-select" id="program" name="programme">
+                    <option value="data engineering" selected>Data Engineering</option>
+                    <option value="software engineering">Software Engineering</option>
+                    <option value="network and security">Network and Security</option>
+                    <option value="bioinformatics">Bioinformatics</option>
+                    <option value="graphic and multimedia">Graphic and Multimedia</option>
                 </select>
             </div>
             <div class="mb-3">
                 <label for="year" class="form-label">Year of Study:</label>
-                <select class="form-select" id="year">
+                <select class="form-select" id="year" name="year">
                     <option value="1" selected>Year 1</option>
                     <option value="2">Year 2</option>
                     <option value="3">Year 3</option>
@@ -115,6 +124,7 @@ if(!isset ($_SESSION['username'])){
             </div>
         </form>
     </div>
+
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
